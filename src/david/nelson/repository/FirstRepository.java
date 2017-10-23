e9a2d674fdd109bc1455d57714c96651ec3f2822
David Nelson <david.nelson36@pcc.edu>
2017-10-22 19:47:31 -0700

    Added main method printing statement and question

--- src/david/nelson/repository/FirstRepository.java
+++ src/david/nelson/repository/FirstRepository.java
@@ -1,4 +1,19 @@
 package david.nelson.repository;
 
-public class FirstRepository {
+/**
+ * This is a class for my course in Java 233j
+ *
+ * @author David Nelson
+ * @version 2017.10.21
+ */
+public class FirstRepository
+{
+    /**
+     * print statement and question.
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, this is my first git repository.");
+        System.out.println("Are we going to be working out of these this course?");
+    }
 }
